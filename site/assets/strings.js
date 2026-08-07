bfd70658f590c548643140fd9fceb1fd1fd7dbfe
Zakk <zakk@gentoozh.org>
2026-08-07 19:01:55 +1000

    site: link the Live ISO download page from the nav (#176)
    因为 Live ISO 与二进制包、distfiles 一样是本站提供的下载，所以导航里要有入口，
    放在配置左边。链接指向 iso.gentoozh.org，那是 ISO 的下载页。

--- site/assets/strings.js
+++ site/assets/strings.js
@@ -1,7 +1,7 @@
 window.MIRROR_I18N_COMMON = {
   'zh-tw': {
     brand: 'distfiles.gentoozh.org',
-    navSetup: '設定', navPkgs: '套件列表', navFiles: '檔案',
+    navIso: 'Live ISO', navSetup: '設定', navPkgs: '套件列表', navFiles: '檔案',
     fSrc: '原始碼', fKey: '簽章公鑰', fDesign: '設計語言', fCommunity: 'Gentoo 中文社群',
     fCommunityUrl: 'https://gentoozh.org/zh-tw/',
     skip: '跳到正文',
@@ -9,7 +9,7 @@ window.MIRROR_I18N_COMMON = {
   },
   'en': {
     brand: 'distfiles.gentoozh.org',
-    navSetup: 'Setup', navPkgs: 'Packages', navFiles: 'Files',
+    navIso: 'Live ISO', navSetup: 'Setup', navPkgs: 'Packages', navFiles: 'Files',
     fSrc: 'Source', fKey: 'Signing key', fDesign: 'Design', fCommunity: 'Gentoo Chinese Community',
     fCommunityUrl: 'https://gentoozh.org/en/',
     skip: 'Skip to content',
